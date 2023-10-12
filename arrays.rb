7558c4b86c4636e10429f8a26fb3c29c83f6873e
Ellis velandia <eyis619@gmail.com>
2023-10-12 17:28:58 -0500

    arrays

---
+++ arrays.rb
@@ -0,0 +1,8 @@
+friends = Array["Ellis", "Karen", "Oscar", "Andy"]
+puts friends
+puts friends[0]
+friends[0] = "Triple H" 
+puts friends.length()
+puts friends.include? "Karen"
+puts friends.reverse()
+puts friends.sort()
