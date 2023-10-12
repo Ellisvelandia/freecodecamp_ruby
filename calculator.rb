5371b975cd20f88ffda63af1b5d3d28b3b74362e
Ellis velandia <eyis619@gmail.com>
2023-10-12 17:14:19 -0500

    calculator

---
+++ calculator.rb
@@ -0,0 +1,8 @@
+puts "Enter a number: "
+num1 = gets.chomp().to_i
+puts "Enter a nother number"
+num2 = gets.chomp().to_i
+
+puts (num1 + num2)
+# puts (num1.to_i + num2.to_i)
+# puts (num1.to_f + num2.to_f)
