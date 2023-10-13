1ec7a528c29b95d07630cc299ab043b3748eb72c
Ellis velandia <eyis619@gmail.com>
2023-10-12 20:18:48 -0500

    statement

---
+++ statements.rb
@@ -0,0 +1,10 @@
+ismale = true
+istall = false
+
+if ismale and istall
+    puts "you are tall male"
+elsif
+    puts "You are a tall male"    
+else
+    puts "you either not male or nor tall or both"
+end        
